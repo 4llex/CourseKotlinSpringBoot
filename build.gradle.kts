3a47ff2c037416520b32dcf018f473f12a768ffd
Alex Rosa <alexr@gec.inatel.br>
2023-10-17 21:18:19 -0300

    Added dependency for flyway migration lib and added SQL command to create customer table in DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("mysql:mysql-connector-java:8.0.28")
+	implementation("org.flywaydb:flyway-mysql")
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
