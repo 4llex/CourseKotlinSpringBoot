5275f5fed639fc6f829da67db8efc31634a3b41a
Alex Rosa <alexr@gec.inatel.br>
2023-11-21 10:21:25 -0300

    Desenvolvendo o Purchase Mapper

--- src/main/kotlin/com/marketbook/controller/PurchaseController.kt
+++ src/main/kotlin/com/marketbook/controller/PurchaseController.kt
@@ -1,6 +1,8 @@
 package com.marketbook.controller
 
+import com.marketbook.controller.mapper.PurchaseMapper
 import com.marketbook.controller.request.PostPurchaseRequest
+import com.marketbook.service.PurchaseService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/purchase")
-class PurchaseController {
+class PurchaseController(
+    private val purchaseService: PurchaseService,
+    private val purchaseMapper: PurchaseMapper
+) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun purchase(@RequestBody request: PostPurchaseRequest) {
-
+        purchaseService.create(purchaseMapper.toModel(request))
     }
 
 }
---
+++ src/main/kotlin/com/marketbook/service/PurchaseService.kt
@@ -0,0 +1,14 @@
+package com.marketbook.service
+
+import com.marketbook.model.PurchaseModel
+import org.springframework.stereotype.Service
+
+@Service
+class PurchaseService {
+
+    fun create(purchaseModel: PurchaseModel) {
+        
+    }
+
+
+}
