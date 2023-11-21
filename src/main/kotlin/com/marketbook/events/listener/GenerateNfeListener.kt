44d5051bae4aaf6d36f8f93884b8248c50244f83
Alex Rosa <alexr@gec.inatel.br>
2023-11-21 11:35:22 -0300

    Deixando os eventos assincronos

--- src/main/kotlin/com/marketbook/events/listener/GenerateNfeListener.kt
+++ src/main/kotlin/com/marketbook/events/listener/GenerateNfeListener.kt
@@ -3,6 +3,7 @@ package com.marketbook.events.listener
 import com.marketbook.events.PurchaseEvent
 import com.marketbook.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -11,6 +12,7 @@ class GenerateNfeListener(
     private val purchaseService: PurchaseService
 ) {
 
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
         val nfe = UUID.randomUUID().toString()
--- src/main/kotlin/com/marketbook/events/listener/UpdateSoldBookListener.kt
+++ src/main/kotlin/com/marketbook/events/listener/UpdateSoldBookListener.kt
@@ -4,6 +4,7 @@ import com.marketbook.events.PurchaseEvent
 import com.marketbook.service.BookService
 import com.marketbook.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -12,6 +13,7 @@ class UpdateSoldBookListener(
     private val bookService: BookService
 ) {
 
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
         bookService.purchase(purchaseEvent.purchaseModel.books)
